7fae457b6fa5481ccbc73c23de91b7437a879c17
alivxo <avsal0218@gmail.com>
2024-07-03 08:39:19 -0400

    edited SQL table

---
+++ database/AlterTables.sql
@@ -0,0 +1,8 @@
+alter table entry 
+Rename column categoryId to EntryID;
+
+alter table entry 
+Drop column name;
+
+alter table entry 
+Rename column description to Description;
---
+++ database/AlteringEntryTable.sql
@@ -0,0 +1,12 @@
+ALTER table entry
+Add	Date varchar (255);
+
+alter table entry
+Add Time varchar (255);
+
+alter table entry 
+Add Vender varchar (255);
+
+alter table entry
+Add Amount varchar (255);
+
